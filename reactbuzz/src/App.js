c46e9e6fb9dd1538e705e8703df8462003d5d430
akashp369 <aakashprajapat59@gmail.com>
2023-07-14 17:20:54 +0530

    assest update

--- reactbuzz/src/App.js
+++ reactbuzz/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import  Navbar  from "./component/Navbar1";
-import Allroute from './component/Allroute';
+
 
 function App() {
   return (
     <div className="App">
-        
+        <h1>Hello world</h1>
     </div>
   );
 }
--- reactbuzz/src/index.js
+++ reactbuzz/src/index.js
@@ -4,18 +4,10 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider  } from "@chakra-ui/react";
-import { Provider } from "react-redux";
-import store from './redux store/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
- 
-        <App />
-        
-
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
